46aeca9e2bbd839783e0eb9a0bc2474156f27747
KURASHIKI Satoru <lurdan@gmail.com>
2016-02-19 08:59:37 +0000

    move volume config from dockerfile to runscript.

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ FROM debian:sid
 MAINTAINER KURASHIKI Satoru <lurdan@gmail.com>
 
 WORKDIR "$HOME"
-VOLUME /etc/group:/etc/group:ro /etc/passwd:/etc/passwd:ro /etc/shadow:/etc/shadow:ro /etc/sudoers.d:/etc/sudoers.d:ro /tmp/.X11-unix:/tmp/.X11-unix:rw /dev/shm:/dev/shm
-
 RUN apt update && apt install -y goldencheetah && apt-get clean
 
 ENTRYPOINT [ "Goldencheetah" ]
--- goldencheetah.sh
+++ goldencheetah.sh
@@ -6,8 +6,14 @@ docker run -it \
        --env="DISPLAY=unix$DISPLAY" \
        --volume="/home/$USER:/home/$USER" \
        --volume=$HOME/.goldencheetah/:$HOME/.goldencheetah \
+       --volume="/etc/group:/etc/group:ro" \
+       --volume="/etc/passwd:/etc/passwd:ro" \
+       --volume="/etc/shadow:/etc/shadow:ro" \
+       --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+       --volume="/dev/shm:/dev/shm" \
        --name goldencheetah \
        --device /dev/snd \
-       --device /dev/usb \
+       --device /dev/ttyUSB0 \
        lurdan/goldencheetah
 
